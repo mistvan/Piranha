7118a879188214cadf137e29aa8db227cc865cb2
Miroslav Istvan <miroslav.istvan@gmail.com>
2015-02-25 07:24:17 +0100

    Stop handle account/login url.

--- Piranha/WebPages/WebPiranha.cs
+++ Piranha/WebPages/WebPiranha.cs
@@ -217,11 +217,11 @@ namespace Piranha.WebPages
 		/// </summary>
 		public static void Init() {
 			// Register the basic account route
-			if (!Config.PassiveMode) {
-				try {
-					RouteTable.Routes.MapRoute("Account", "account/{action}", new { controller = "auth", action = "index" }, new string[] { "Piranha.Web" }) ;
-				} catch {}
-			}
+            //if (!Config.PassiveMode) {
+            //    try {
+            //        RouteTable.Routes.MapRoute("Account", "account/{action}", new { controller = "auth", action = "index" }, new string[] { "Piranha.Web" }) ;
+            //    } catch {}
+            //}
 
 			// Register hostnames
 			RegisterDefaultHostNames() ;
